d65a908c5f6d8cc067786b5a2838c38946b2b1f1
Chris Atkin <atkinchris@gmail.com>
2017-10-01 14:36:39 +0100

    Fix user data for Amazon sudo

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,11 +1,9 @@
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+#!/bin/bash
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+yum update -y
+yum install -y git docker
+service docker start
+usermod -a -G docker ec2-user
 
-cd ~
-git clone https://github.com/atkinchris/custom-iso.git
-cd custom-iso
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
