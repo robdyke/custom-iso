80c934d9f7958d9a99b3f23e274d66f999b62f70
Chris Atkin <atkinchris@gmail.com>
2017-10-23 11:15:45 +0100

    Condense Polybars

--- config/config/includes.chroot/etc/skel/.config/polybar/launch.sh
+++ config/config/includes.chroot/etc/skel/.config/polybar/launch.sh
@@ -6,8 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch top and bottom bars
+# Launch bars
 polybar top &
-polybar bottom &
 
 echo "Bars launched..."
